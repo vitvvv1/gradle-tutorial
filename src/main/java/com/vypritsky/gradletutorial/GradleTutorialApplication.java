2a0dafbb944f0f492a6eb0b0d3e1f5237185cecf
Vit <vitvvv1@gmail.com>
2018-05-15 22:27:47 +0300

    Init gradle tutorial

---
+++ src/main/java/com/vypritsky/gradletutorial/GradleTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.vypritsky.gradletutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTutorialApplication.class, args);
+	}
+}
